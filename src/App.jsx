a7d8e6fac9776c4f7cf1c4fe66be4bfc26dbb4a1
Ayush Bhosale <bhosaleayush45@gmail.com>
2026-09-06 18:55:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import About from './Components/About/About'
+import Home from './Components/Home/Home'
+import Contact from './Components/Contact/Contact'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+
+const App = () =>{
+
+    return(
+
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+        </BrowserRouter>
+    )
+}
+
+export default App
